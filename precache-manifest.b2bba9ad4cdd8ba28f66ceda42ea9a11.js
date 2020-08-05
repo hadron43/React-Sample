ca59804c6e608f33a7dc8eedd6e0ccdaf1d51eb4
Harsh Kumar <harsh19043@iiitd.ac.in>
2020-08-05 23:45:09 +0530

    Updates

---
+++ precache-manifest.b2bba9ad4cdd8ba28f66ceda42ea9a11.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc15150020de4a408607d9c35cab957",
+    "url": "/React-Sample/index.html"
+  },
+  {
+    "revision": "b4161820236333208677",
+    "url": "/React-Sample/static/css/2.accba2c8.chunk.css"
+  },
+  {
+    "revision": "6a0a1d81d51f09fdd8f4",
+    "url": "/React-Sample/static/css/main.f9fadfa4.chunk.css"
+  },
+  {
+    "revision": "b4161820236333208677",
+    "url": "/React-Sample/static/js/2.cf95ba39.chunk.js"
+  },
+  {
+    "revision": "3a5402f5ddd536a8f5deb8d906cf7b41",
+    "url": "/React-Sample/static/js/2.cf95ba39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a0a1d81d51f09fdd8f4",
+    "url": "/React-Sample/static/js/main.c5dd761b.chunk.js"
+  },
+  {
+    "revision": "f29bee52b67182012b08",
+    "url": "/React-Sample/static/js/runtime-main.a5c09d7d.js"
+  },
+  {
+    "revision": "43b3152813e3b8a2f652289856fd474f",
+    "url": "/React-Sample/static/media/airbus.43b31528.43b31528.svg"
+  },
+  {
+    "revision": "5ef6a897bfbfd0a9a6b880ef25d4d50c",
+    "url": "/React-Sample/static/media/apple.5ef6a897.5ef6a897.svg"
+  },
+  {
+    "revision": "da3f480e83a1347ab301079b24d4bb19",
+    "url": "/React-Sample/static/media/dekra.da3f480e.da3f480e.svg"
+  },
+  {
+    "revision": "cc8c0725862922e505d620059a800dfc",
+    "url": "/React-Sample/static/media/google-2015.cc8c0725.svg"
+  },
+  {
+    "revision": "d27874a76a39d36812e73e9bda42de7d",
+    "url": "/React-Sample/static/media/hubspot.d27874a7.d27874a7.svg"
+  },
+  {
+    "revision": "a768e74f114d5a13e05af7297f1d348b",
+    "url": "/React-Sample/static/media/join.a768e74f.svg"
+  },
+  {
+    "revision": "3900bd895ac3bb2dd0a05ee8a9335ef4",
+    "url": "/React-Sample/static/media/meeting.3900bd89.svg"
+  },
+  {
+    "revision": "f44dbbaadcdaded90700dbfcbdc40090",
+    "url": "/React-Sample/static/media/profit.f44dbbaa.svg"
+  },
+  {
+    "revision": "b8af1e94ace4a7ae190a888090e45e51",
+    "url": "/React-Sample/static/media/resume.b8af1e94.svg"
+  },
+  {
+    "revision": "3902ddb22ddafe389592bef6b597f4b4",
+    "url": "/React-Sample/static/media/search.3902ddb2.svg"
+  },
+  {
+    "revision": "6433f9c4f84c9f64418c08ecabdccab8",
+    "url": "/React-Sample/static/media/staff.6433f9c4.svg"
+  },
+  {
+    "revision": "987327a235762a672d033e070014af0b",
+    "url": "/React-Sample/static/media/statistics.987327a2.svg"
+  }
+]);
